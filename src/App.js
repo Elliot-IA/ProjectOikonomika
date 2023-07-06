b6fa50a217197974a12562b1f25c5848a9e69746
blake-hokanson <blake.hokanson@icloud.com>
2023-07-06 09:19:29 -0500

    initial labels

--- src/App.js
+++ src/App.js
@@ -15,12 +15,47 @@ function App() {
   return (
     <div className="App">
       <div className="textHeader">Supply & Demand Live</div>
-      <div className="container" style={size}>
-        <VerticleGridLine y={width} n={numberY}></VerticleGridLine>
-        <HorizontialGridLine x={width} n={numberX}></HorizontialGridLine>
-        <Line id={"line1"} angle={45} x={0} y={500} l={600}></Line>
-        <Line id={"line2"} angle={-45} x={0} y={500} l={600}></Line>
-        <Point trackID={["line1", "line2"]}></Point>
+
+      <div style={{ display: "Grid", gridTemplateColumns: "30px auto", gap: "2px" }}>
+        <div
+          style={{
+            width: "30px",
+            height: size.height,
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <div style={{ height: "100px" }}></div>
+          {[...Array(7).keys()].map((n) => (
+            <div key={n} style={{ textAlign: "right", height: "100px" }}>
+              {7 - n}
+            </div>
+          ))}
+        </div>
+        <div className="container" style={size}>
+          <VerticleGridLine y={width} n={numberY}></VerticleGridLine>
+          <HorizontialGridLine x={width} n={numberX}></HorizontialGridLine>
+          <Line id={"line1"} angle={45} x={0} y={500} l={600}></Line>
+          <Line id={"line2"} angle={-45} x={0} y={500} l={600}></Line>
+          <Point trackID={["line1", "line2"]}></Point>
+        </div>
+        <div style={{ width: "30px", height: "30px" }}></div>
+        <div
+          style={{
+            width: size.height,
+            height: "30px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <div style={{ width: "100px" }}></div>
+          {[...Array(7).keys()].map((n) => (
+            <div key={n} style={{ width: "100px" }}>
+              {n + 1}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
