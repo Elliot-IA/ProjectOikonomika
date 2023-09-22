654c125c03b499237f057070021be4bb295d882f
ianalexanderrje <alexa818@umn.edu>
2023-09-22 13:50:46 -0500

    sdfs

--- src/App.js
+++ src/App.js
@@ -10,69 +10,73 @@ import ReactGA from 'react-ga4';
 const TRACKING_ID = "G-R9K8K9656B"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
 ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
+    hitType: "pageview",
+    page: window.location.pathname,
 });
+var loading = true;
 
 function App() {
-  var new_iframe = document.createElement("iframe");
-  document.body.appendChild(new_iframe);
-  new_iframe.style.display = "none";
-  new_iframe.src="https://alertzy.app/send?accountKey=m143fegulr79ila&title=Fredrich Graph Program Sniff&message=someone loaded up econ graph!";
-  setTimeout(()=>{new_iframe.remove()},1000);
-
-
-
-  const [envObject, setEnv] = useState(env);
-
-  useEffect(() => {
-    const urlParams = queryString.parse(window.location.search);
-    Object.keys(urlParams).forEach((key) => {
-      if (key === "name") urlParams[key] = urlParams[key]
-      else if (key === "macro") urlParams[key] = (urlParams[key]) === 'true'
-      else urlParams[key] = Number(urlParams[key])
-    });
-    if (Object.keys(urlParams).length > 0) {
-      setEnv({ ...urlParams });
+    if(loading){
+        var new_iframe = document.createElement("iframe");
+        document.body.appendChild(new_iframe);
+        new_iframe.style.display = "none";
+        new_iframe.src="https://alertzy.app/send?accountKey=m143fegulr79ila&title=Fredrich Graph Program Sniff&message=someone loaded up econ graph!";
+        setTimeout(()=>{new_iframe.remove()},1000);
+        loading = false;
     }
-  }, []);
-
-  const [showSettings, changeShowSettings] = useState(false)
-
-  return (
-    <div className="App" style={{ display: "grid", placeItems: "center" }}>
-
-      <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-        <button style={{ width: '55px', visibility: 'hidden' }}></button>
 
 
-        <div className="not-selectable" style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20px", marginBottom: "20px" }}>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            {!envObject.macro && <h1 style={{ margin: "0px", width: "200px", textAlign: "right" }} >Supply</h1>}
-            {envObject.macro && <h1 style={{ margin: "0px", width: "300px", textAlign: "right" }} >Aggregate Supply</h1>}
-            <h1 style={{ margin: "0px 10px 0px 10px" }}>&</h1>
-            {!envObject.macro && <h1 style={{ margin: "0px", width: "200px", textAlign: "left" }} >Demand</h1>}
-            {envObject.macro && <h1 style={{ margin: "0px", width: "300px", textAlign: "left" }} >Aggregate Demand</h1>}
-          </div>
-          <h1 style={{ margin: "0px" }}>Live!</h1>
-        </div>
+
+    const [envObject, setEnv] = useState(env);
+
+    useEffect(() => {
+        const urlParams = queryString.parse(window.location.search);
+        Object.keys(urlParams).forEach((key) => {
+            if (key === "name") urlParams[key] = urlParams[key]
+            else if (key === "macro") urlParams[key] = (urlParams[key]) === 'true'
+            else urlParams[key] = Number(urlParams[key])
+        });
+        if (Object.keys(urlParams).length > 0) {
+            setEnv({ ...urlParams });
+        }
+    }, []);
+
+    const [showSettings, changeShowSettings] = useState(false)
+
+    return (
+        <div className="App" style={{ display: "grid", placeItems: "center" }}>
+
+        <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+            <button style={{ width: '55px', visibility: 'hidden' }}></button>
 
 
-        <img src={require('./pictures/icon.png')} onClick={() => changeShowSettings(!showSettings)} style={{ width: "35px", height: "35px", marginTop: "20px", marginRight: "20px" }} />
+<div className="not-selectable" style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: "20px", marginBottom: "20px" }}>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+        {!envObject.macro && <h1 style={{ margin: "0px", width: "200px", textAlign: "right" }} >Supply</h1>}
+{envObject.macro && <h1 style={{ margin: "0px", width: "300px", textAlign: "right" }} >Aggregate Supply</h1>}
+<h1 style={{ margin: "0px 10px 0px 10px" }}>&</h1>
+{!envObject.macro && <h1 style={{ margin: "0px", width: "200px", textAlign: "left" }} >Demand</h1>}
+{envObject.macro && <h1 style={{ margin: "0px", width: "300px", textAlign: "left" }} >Aggregate Demand</h1>}
+</div>
+<h1 style={{ margin: "0px" }}>Live!</h1>
+</div>
 
 
-      </div>
-
-      <div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
-        <div style={{ width: '320px' }}></div>
-        <Graph env={envObject}></Graph>
-        <Settings env={envObject} setEnv={setEnv} show={showSettings} setShow={changeShowSettings}></Settings>
+<img src={require('./pictures/icon.png')} onClick={() => changeShowSettings(!showSettings)} style={{ width: "35px", height: "35px", marginTop: "20px", marginRight: "20px" }} />
 
 
-      </div>
+</div>
 
-    </div >
-  );
+<div style={{ display: "flex", justifyContent: "space-between", width: "100%" }}>
+    <div style={{ width: '320px' }}></div>
+<Graph env={envObject}></Graph>
+<Settings env={envObject} setEnv={setEnv} show={showSettings} setShow={changeShowSettings}></Settings>
+
+
+</div>
+
+</div >
+);
 }
 
 export default App;
